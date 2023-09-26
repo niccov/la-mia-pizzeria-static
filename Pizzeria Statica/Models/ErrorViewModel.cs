3decc4605e17514ef1ce6d0cfc833536113765d6
niccov <niccoventuri27@gmail.com>
2023-09-26 15:56:12 +0200

    Aggiungere i file di progetto.

---
+++ Pizzeria Statica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pizzeria_Statica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
